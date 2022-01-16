d7821f1bc139c8d1ff3ec3d7f525cf61099ee7ed
vimalkmr816 <vimalkmr816@gmail.com>
2022-01-16 13:32:01 +0530

    final commit

--- toast notification/main.js
+++ toast notification/main.js
@@ -1,6 +1,16 @@
-const popUp = document.querySelector('.pop_up');
+const btn = document.querySelector('.btn');
+const popUp = document.querySelector('.pop');
+btn.addEventListener("click", () => {
+    createNotification();
+});
 
-popUp.addEventListener('click', () => {
-    popUp.classList.toggle('active');
-    console.log('Hello')
-})
+function createNotification() {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.innerText = 'You have a new notification!';
+    container.appendChild(notif);
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+
+}
