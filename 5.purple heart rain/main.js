cdaa0eaf0f36eefd159f3c83409e07ab294860e8
vimalkmr816 <vimalkmr816@gmail.com>
2022-01-18 18:52:36 +0530

    heart rain completed

---
+++ 5.purple heart rain/main.js
@@ -0,0 +1,9 @@
+function createHeart() {
+    const heart = document.createElement('div')
+    heart.classList.add('heart');
+    heart.innerText = '💜';
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    document.body.appendChild(heart);
+}
+setInterval(createHeart, 300)
